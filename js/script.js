832bc2b42023567260f7cb12a3fbcd8afe92d39c
Zoey Kariyal <zoey.kariyal@stu.ocsb.ca>
2025-06-16 15:04:50 +0000

    made a web page called and about Genesis

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+"use strict"
+// This function calculates the sum of all numbers from 0 to the user input number
+
+function repeatSentence () {
+
+    let counter = 0
+
+    let finalSentences = ""
+
+    let sentence = document.getElementById("userSentence").value
+
+    let userNum = parseInt(document.getElementById("userNum").value)
+
+    do {
+        finalSentences = finalSentences + sentence + "<br>"
+
+        counter = counter + 1
+    }while (counter < userNum)
+
+    document.getElementById("display-results").innerHTML = finalSentences
+}
